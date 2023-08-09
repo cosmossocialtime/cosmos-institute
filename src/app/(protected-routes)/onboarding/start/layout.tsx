15b52dfe8cfcf6cc92ab5b76edf904878b030230
ximeen <gabriximenes100@gmail.com>
2023-08-08 22:11:09 -0300

    :up: ajusts

--- src/app/(protected-routes)/onboarding/start/layout.tsx
+++ src/app/(protected-routes)/onboarding/start/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-  title: 'Rapaz é um teste oo',
+  title: 'Início',
 }
 
 export default async function PrivateRoutes({
--- src/app/(protected-routes)/onboarding/start/page.tsx
+++ src/app/(protected-routes)/onboarding/start/page.tsx
@@ -1,16 +1,12 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 
 export default function Teste() {
+  function handleNextPage() {
+    console.log('Hello')
+  }
+
   return (
-    <main className="h-screen w-full items-center justify-center">
-      <Image
-        src="/onboardingStart.png"
-        width={1000}
-        height={1000}
-        alt="background cosmonauta"
-        className="relative h-full w-full bg-cover bg-no-repeat"
-      />
+    <main className="h-screen w-full items-center justify-center bg-backgroundStart bg-cover bg-no-repeat">
       <div className="absolute left-1/3 top-1/2 flex max-w-xl -translate-x-1/2 -translate-y-1/2 flex-col gap-7 rounded-2xl bg-[#7C7C7C]/10 p-14 backdrop-blur-md">
         <h1 className="text-3xl font-semibold text-zinc-50">
           Olá, Cosmonauta!
@@ -19,7 +15,9 @@ export default function Teste() {
           Boas-vindas ao programa [Nome do Programa]. Prepare-se para uma
           aventura!
         </span>
-        <Button className="py-6 text-lg">Vamos lá!</Button>
+        <Button onClick={handleNextPage} className="py-6 text-lg">
+          Vamos lá!
+        </Button>
       </div>
     </main>
   )
